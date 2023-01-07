926368069de39660117d3cf755b89996341aa27e
Orhu <mcdonoughmalcolm@gmail.com>
2023-01-06 18:50:01 -0600

    update on tick interface fix

--- Assets/Scripts/Obstacles/IUpdateOnTick.cs
+++ Assets/Scripts/Obstacles/IUpdateOnTick.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IUpdateOnTick : MonoBehaviour {
+public interface IUpdateOnTick {
     void OnTick();
 }
