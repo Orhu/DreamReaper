1e1acd93a5a6c305505c431f8be97f153450cf8e
Orhu <mcdonoughmalcolm@gmail.com>
2023-01-06 18:06:08 -0600

    watchers cant get phased through

--- Assets/Scripts/Obstacles/Enemies/WatcherAI.cs
+++ Assets/Scripts/Obstacles/Enemies/WatcherAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WatcherAI : MonoBehaviour, IObstacle {
-    public bool phasable {get; private set;} = true;
+    public bool phasable {get; private set;} = false;
     public int type {get; private set;} = 2; // type key: 0 = wall, 1 = door, 2 = enemy, 3 = prop
 
     // Start is called before the first frame update
